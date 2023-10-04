b418d4c4c1f851943e74ea1e5a3f4db163ab3d62
Torben Schweren <torben@schweren.dev>
2023-10-04 20:27:09 +0200

    Improved struct example

--- generics/src/deduplication_struct_by_generics.rs
+++ generics/src/deduplication_struct_by_generics.rs
@@ -1,39 +1,51 @@
 //Bad:
 
+#[derive(Debug)]
 struct PointWithIntegers {
     _x: i32,
     _y: i32,
 }
 
+#[derive(Debug)]
 struct PointWithFloats {
     _x: f32,
     _y: f32,
 }
 
 pub fn bad() {
-    let _p1 = PointWithIntegers { _x: 5, _y: 10 };
-    let _p2 = PointWithFloats { _x: 1.0, _y: 4.0 };
-    let _p3 = PointWithIntegers { _x: 5, _y: 10 };
+    let p1 = PointWithIntegers { _x: 5, _y: 10 };
+    let p2 = PointWithFloats { _x: 1.0, _y: 4.0 };
+    let p3 = PointWithIntegers { _x: 5, _y: 10 };
+    println!("p1: {:?}", p1);
+    println!("p2: {:?}", p2);
+    println!("p3: {:?}", p3);
 }
 
 //====================================================================================================
 
 //Good:
 
+#[derive(Debug)]
 struct Point<T> {
     _x: T,
     _y: T,
 }
 
+#[derive(Debug)]
 struct CoolerPoint<T, U> {
     _x: T,
     _y: U,
 }
 
 pub fn good() {
-    let _p1 = Point { _x: 5, _y: 10 };
-    let _p2 = Point { _x: 1.0, _y: 4.0 };
-    let _p3 = Point { _x: 5, _y: 10 };
+    let p1 = Point { _x: 5, _y: 10 };
+    let p2 = Point { _x: 1.0, _y: 4.0 };
+    let p3 = Point { _x: 5, _y: 10 };
     //let _wont_work = Point { x: 5, y: 4.0 };
-    let _will_work = CoolerPoint { _x: 5, _y: 4.0 };
+    let will_work = CoolerPoint { _x: 5, _y: 4.0 };
+
+    println!("p1: {:?}", p1);
+    println!("p2: {:?}", p2);
+    println!("p3: {:?}", p3);
+    println!("will_work: {:?}", will_work);
 }
