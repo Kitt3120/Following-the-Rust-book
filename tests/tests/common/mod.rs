a0225eb0c48d703f4f5675376b2aa9935a62f79e
Torben Schweren <torben@schweren.dev>
2023-10-11 22:40:04 +0200

    Add example for common helper functions in tests

---
+++ tests/tests/common/mod.rs
@@ -0,0 +1,3 @@
+pub fn setup() {
+    println!("The setup function has been called!");
+}
--- tests/tests/example_integration_test.rs
+++ tests/tests/example_integration_test.rs
@@ -4,9 +4,11 @@
     using only the public API and potentially exercising multiple modules per test.
 */
 
-use example; // Notice that despite the directory being called tests, we use example here, as defined in Cargo.toml
+use example; // Notice that despite the directory being called tests, we use example here, as defined in Cargo.toml // Code for helper functions can be seperated into submodules so that they won't be interpreted as tests
+mod common;
 
 #[test]
 fn test_add_externally() {
+    common::setup(); // Helper function that is shared between tests
     assert_eq!(4, example::add(2, 2));
 }
